86c9bb3f015d477197f9340f5af50d71e8dc5e42
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2025-05-30 10:07:19 +0200

    added gradient to name

--- git-practic/app/page.tsx
+++ git-practic/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-6xl text-white font-bold ">
+        <h1 className="text-6xl  font-extrabold bg-gradient-to-r from-purple-100 to-purple-500 bg-clip-text text-transparent ">
           Ernesto Carocca 
 
         </h1>
