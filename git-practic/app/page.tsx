3bd3cfcd03934043a72227bfa0fecc36dce1e6db
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2025-05-30 09:55:11 +0200

    installed next.js

---
+++ git-practic/app/page.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-6xl text-white font-bold ">
+          Ernesto Carocca 
+
+        </h1>
+        
+        <p className="text-blue-700 text-3xl font-extrabold">Git-Practis</p>
+    </main>
+</div>
+
+
+
+
+  );
+}
